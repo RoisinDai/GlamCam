f82f767231cf3f4e885c07390761b257dfec0bd8
Roisin-Dai <r25dai@uwaterloo.ca>
2026-01-13 19:06:33 -0500

    fix live video feed not showing up in launcher script

--- website/frameProducer.py
+++ website/frameProducer.py
@@ -99,9 +99,16 @@ def main() -> None:
                     length = len(data)
                     try:
                         conn.sendall(length.to_bytes(4, "big") + data)
-                    except Exception as e:
-                        print("Connection closed.")
-                        break
+                    except Exception:
+                        # do not change this section!!!
+                        print("Consumer disconnected; waiting for reconnect...")
+                        try:
+                            conn.close()
+                        except:
+                            pass
+                        conn, addr = server.accept()
+                        print(f"Consumer reconnected from {addr}.")
+                        continue
 
                     # Display the processed frame individually
                     cv2.imshow("Processed Frame", processed_frame)
