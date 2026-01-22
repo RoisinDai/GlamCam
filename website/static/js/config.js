130a23af885f29cffa8fd02cc678b09a97f9e4dc
Roisin-Dai <roisindai@outlook.com>
2026-01-21 22:47:42 -0500

    new refactored frontend (many changes combined in one!!!)

---
+++ website/static/js/config.js
@@ -0,0 +1,41 @@
+// Shared config/data for the frontend
+window.CLOSET_CATEGORIES = [
+  { key: "Tops", img: "/static/Tops.PNG", alt: "Tops" },
+  { key: "Bottoms", img: "/static/Bottoms.PNG", alt: "Bottoms" },
+  { key: "Dresses", img: "/static/Dresses.PNG", alt: "Dresses" },
+  { key: "Hats", img: "/static/Hats.PNG", alt: "Hats" },
+  { key: "Fullbody", img: "/static/Fullbody.PNG", alt: "Fullbody" },
+];
+
+window.CATEGORY_TYPE = {
+  Tops: "top",
+  Bottoms: "bottom",
+  Dresses: "fullbody",
+  Fullbody: "fullbody",
+  Hats: "hat",
+};
+
+window.CLOTHING_NAMES = {
+  Tops: [
+    "mindfront_lusekofta",
+    "punkduck_retro_top",
+    "namuhekam_male_polo_shirt",
+    "toigo_fisherman_sweater",
+    "mindfront_knitted_sweater_01",
+    "mindfront_knitted_sweater_02",
+  ],
+  Bottoms: [
+    "elvs_gored_elephant_pants",
+    "mindfront_male_trousers_2",
+    "elvs_disco_pants_double_ruffles",
+    "sonicteam998_maria_skirt",
+    "elvs_pleated_plaid_mini_skirt",
+  ],
+  Dresses: ["elvs_halter_dress_mini", "punkduck_middle_length_qipao"],
+  Hats: [
+    "jujube_bag_on_head",
+    "culturalibre_cl_don_quixote_hat",
+    "elvs_tophat1",
+  ],
+  Fullbody: ["elvs_emt_uniform_jacket_male"],
+};
