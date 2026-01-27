3dc39943c98591046d03647556d9b1b8b15758c5
Roisin Dai <roisindai@outlook.com>
2026-01-27 18:41:05 -0500

    Make AvatarCamera stationary @origin, matching Kinect FOV

--- GlamCam/Assets/Scripts/AvatarView.cs
+++ GlamCam/Assets/Scripts/AvatarView.cs
@@ -4,28 +4,21 @@ using System.Collections.Generic;
 using Kinect = Windows.Kinect;
 using Vector3 = UnityEngine.Vector3;
 
-// Responsible for controlling the camera
-// pointing at the clothed avatar, which
-// may or may not have the base avatar visible.
+// Creates a 1:1 optical match between Unity's 3D space and the Kinect's video feed
+// This allows avatars to appear correctly aligned over the user's body in the video background.
 public class AvatarView : MonoBehaviour
 {
-  public GameObject AvatarCamera; // The camera that will view the avatar
-  public GameObject ClothedAvatarHips; // The dressed avatar's hips
+  public Camera AvatarCamera;
 
-  // Updates the body objects
-  void Update()
+  void Start()
   {
-    if (AvatarController.trackedBody == null || !AvatarController.trackedBody.IsTracked)
-    {
-      // No body currently being tracked by AvatarController
-      return;
-    }
+    // 1. Considering the Kinect Sensor to be at the "Origin" of the world,
+    //    we place the AvatarCamera at the same location at (0,0,0), looking forward.
+    AvatarCamera.transform.position = Vector3.zero;
+    AvatarCamera.transform.rotation = Quaternion.identity;
 
-    // Get the tracked body from AvatarController
-    Kinect.Body body = AvatarController.trackedBody;
-
-    // Move the Avatar camera to the spine base position of the joint skeleton
-    Vector3 spineBase = BodySourceView.GetVector3FromJoint(body.Joints[Kinect.JointType.SpineBase]);
-    AvatarCamera.transform.position = new Vector3(ClothedAvatarHips.transform.position.x, spineBase.y - 1f, spineBase.z - 40f);
+    // 2. Configure the field of view (FOV) of the AvatarCamera
+    // The Kinect v2 has a vertical FOV of approximately 53.8 degrees according to https://smeenk.com/kinect-field-of-view-comparison/
+    AvatarCamera.fieldOfView = 53.8f;
   }
 }
